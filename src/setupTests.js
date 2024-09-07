ce9e4c8ccce82f96056a917d481f78368c3a5774
vansh4545 <97082701+vansh4545@users.noreply.github.com>
2024-09-07 19:10:02 +0530

    Initial commit

---
+++ src/setupTests.js
@@ -0,0 +1,11 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+//import '@testing-library/jest-dom';
+
+
+ // "@testing-library/jest-dom": "^5.16.5",
+    // "@testing-library/react": "^14.0.0",
+    // "@testing-library/user-event": "^13.5.0"
+    
